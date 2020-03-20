231683582e422b6d461324a2172ce0bf8235cd3b
RakaSulthon <ASanrar92@gmail.com>
2020-03-20 14:19:20 +0700

    design new

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -4,63 +4,121 @@
 
 
 @section('content')
-<div class="container py-3">
-  <div class="row">
-    <div class="col-sm-3 pr-1">
-      <div class="card">
-        <div class="container text-center py-1" style="background-color:#d1d1d1">
-          <h4>ARSIP</h4>
+<style>
+#satu {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: -1;
+}
+</style>
+<section id="satu" style="padding-top:50px">
+  <img src="/img/header2.png" class="d-block w-100" alt="img-responsive"  width="50%">
+
+</section>
+
+<div class="container py-5">
+  <div class="card">
+    <div class="jumbotron-fluid pt-2" style="background-color:white">
+      <img src="/img/header3.png" class="d-block w-100 px-2" alt="img-responsive"  width="50%">
+    </div>
+
+    <div class="row">
+      <div class="col-sm-3 pr-1">
+        <div class="card ml-2 my-2">
+          <div class="container text-center py-1" style="background-color:#d1d1d1">
+            <h4>TERBARU</h4>
+          </div>
+          <div class="row">
+            <div class="col-md-4 text-center py-3">
+              <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body text-sm-left">
+                <h5 class="card-title" href="/" >Berita 1</h5>
+                <p class="card-text">Isi Berita 1</p>
+                <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+              </div>
+            </div>
+
+            <div class="col-md-4 text-center py-3">
+              <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body text-sm-left">
+                <h5 class="card-title" href="/" >Berita 1</h5>
+                <p class="card-text">Isi Berita 1</p>
+                <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+              </div>
+            </div>
+
+          </div>
         </div>
-        <div class="row">
-          <div class="col-md-4 text-center py-3">
-            <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
+        <div class="card ml-2 my-2">
+          <div class="container text-center py-1" style="background-color:#d1d1d1">
+            <h4>POPULER</h4>
           </div>
-          <div class="col-md-8">
-            <div class="card-body text-sm-left">
-              <h5 class="card-title" href="/" >Berita 1</h5>
-              <p class="card-text">Isi Berita 1</p>
-              <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+          <div class="row">
+            <div class="col-md-4 text-center py-3">
+              <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
             </div>
-          </div>
-
-          <div class="col-md-4 text-center py-3">
-            <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body text-sm-left">
-              <h5 class="card-title" href="/" >Berita 1</h5>
-              <p class="card-text">Isi Berita 1</p>
-              <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+            <div class="col-md-8">
+              <div class="card-body text-sm-left">
+                <h5 class="card-title" href="/" >Berita 1</h5>
+                <p class="card-text">Isi Berita 1</p>
+                <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+              </div>
             </div>
-          </div>
 
+            <div class="col-md-4 text-center py-3">
+              <img src="img/logoPPMU.png" class="card-img img-thumbnail rounded mx-auto d-block" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body text-sm-left">
+                <h5 class="card-title" href="/" >Berita 1</h5>
+                <p class="card-text">Isi Berita 1</p>
+                <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-sm-9">
-      <div class="card">
-        <div class="jumbotron d-flex align-items-center text-center" style="width:auto;height:100px;background-color:#d1d1d1">
-          <div class="container ">
-            <h1>{{$artikel->nama_artikel}}</h1>
+      <div class="col-sm-9">
+        <div class="card my-2 mr-2">
+          <div class="jumbotron d-flex align-items-center text-center" style="width:auto;height:100px;background-color:#d1d1d1">
+            <div class="container ">
+              <h1>{{$artikel->nama_artikel}}</h1>
+            </div>
           </div>
-        </div>
-        <div class="container">
           <div class="container">
-                <div class="text-center mx-2">
-                  <img src="/img_artikel/{{$artikel->gambar_artikel}}"  style="width:50%">
-                </div>
-                <div class="text py-3 my-2 mx-2" style="border-box">
-                  <p>{{$artikel->isi_artikel}}</p>
-                </div>
+            <div class="container">
+                  <div class="text-center mx-2">
+                    <img src="/img_artikel/{{$artikel->gambar_artikel}}"  style="width:50%">
+                  </div>
+                  <div class="text py-3 my-2 mx-2" style="border-box">
+                    <p>{{$artikel->isi_artikel}}</p>
+                  </div>
 
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </div>
 
 
+<section id="footer" class="footer">
+  <div class="jumbotron-fluid text-light" style="background:#1B2626" >
+    <div class="container text-center">
+      <div class="footer-copyright text-center py-1">© 2020 Copyright :
+        <a > PPMU </a>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,10 @@
   box-sizing: border-box;
 }
 
+body{
+	background-color:#fff;
+}
+
 .column {
   float: left;
   width: 33.33%;
@@ -37,13 +41,7 @@
 </head>
 
 <body class="data-spy=" scroll" data-target=".navbar" data-offset="50"">
-
-  <div class="container-fluid text-center">
-  <br>
-  <h1>Disini templat leaflet</h1>
-  <br>
-  </div>
-    <nav class="navbar navbar-expand-sm navbar-dark sticky-top font-weight-bold" style="background:#027cd9">
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top font-weight-bold" style="background:#3282B8">
       <div class="container">
       <a class="navbar-brand" href="/">BERANDA</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -62,13 +60,13 @@
             <a class="nav-link" href="/uks/home">UKS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/blog ">Artikel</a>
+            <a class="nav-link" href="/blog ">ARTIKEL</a>
           </li>
 
         </ul>
-        <form class="form-inline my-2 my-lg-0">
+        <form class="form-inline my-sm-0">
           <input class="form-control mr-sm-2" type="search" placeholder=". . . . ." aria-label="Search">
-          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-light my-sm-0" type="submit">Search</button>
         </form>
       </div>
 
@@ -109,7 +107,7 @@
 
   <!-- Footer -->
 
-  <section id="footer" class="footer">
+  <!-- <section id="footer" class="footer">
     <div class="jumbotron-fluid bg-primary text-light" >
       <div class="container text-center">
         <div class="footer-copyright text-center py-1">© 2019 Copyright :
@@ -117,7 +115,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 </body>
 
 
